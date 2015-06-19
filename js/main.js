469091c695002949699d0048b22e6551cb00583e
David Veselý <davidvesely.cz@gmail.com>
2015-06-20 00:35:39 +0200

    Homepage update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('.btn-radio').on('click', function(){
 	$(this).toggleClass('active');
 });
+
+var $progress = $('.progress-bar');
+var $progressFill = $progress.find('.bar-fill');
+var progressFrom = $progress.data('from');
+var progressComplete = $progress.data('complete');
+$progressFill.css('width', progressFrom+'%');
+setTimeout(function() {
+	$progressFill.css('width', progressComplete+'%');
+}, 500);
